3a88bdc777a3acc23ad3dd2774d72204e2f2ed6a
adeonhy <adeonhy@gmail.com>
2015-07-08 12:50:20 +0900

    Initial Commit

---
+++ pagemenu_sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pagemenu_sample
+//
+//  Created by adeonhy on 2015/07/08.
+//  Copyright (c) 2015年 wmegane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
